9d9b8207de3e39686dbaa9933dca79ea3ff86a54
Mysha2009 <shamygounon@gmail.com>
2024-10-08 17:23:23 +0100

    modifs

--- arbre1/src/pages/form1.jsx
+++ arbre1/src/pages/form1.jsx
@@ -93,7 +93,7 @@ const FormElementInput = () => {
           </DefaultColumn>
 
           <DefaultColumn>
-            <Genr />
+            <DeathDate />
           </DefaultColumn>
         </div>
       </div>
@@ -106,222 +106,221 @@ export default FormElementInput;
 const DefaultColumn = ({ children }) => {
   return (
     <div className='w-full px-4 md:w-1/2 lg:w-1/3'>
-      <div className='mb-12'>{children}</div>
+      <div className='mb-4 mr-8 ml-8'>{children}</div>
     </div>
+
+    
   )
 }
 
-const NomComplet = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Nom complet:
-      </label>
-      <input
-        type='text'
-        placeholder='Your name'
-        className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
-      />
-    </>
-  )
-}
+  const NomComplet = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Nom complet:
+        </label>
+        <input
+          type='text'
+          placeholder='Your name'
+          className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
+        />
+      </>
+    )
+  }
 
-const Nationalite = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Nationalite
-      </label>
-      <div className='relative z-20'>
-        <select className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'>
-          <option value='' className='dark:bg-dark-2'>Benin</option>
-          <option value='' className='dark:bg-dark-2'>Togo</option>
-          <option value='' className='dark:bg-dark-2'>Chine</option>
-        </select>
-        <span className='absolute right-4 top-1/2 z-10 mt-[-2px] h-[10px] w-[10px] -translate-y-1/2 rotate-45 border-r-2 border-b-2 border-body-color'></span>
-      </div>
-    </>
-  )
-}
+  const Nationalite = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Nationalite
+        </label>
+        <div className='relative z-20'>
+          <select className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'>
+            <option value='' className='dark:bg-dark-2'>Benin</option>
+            <option value='' className='dark:bg-dark-2'>Togo</option>
+            <option value='' className='dark:bg-dark-2'>Chine</option>
+          </select>
+          <span className='absolute right-4 top-1/2 z-10 mt-[-2px] h-[10px] w-[10px] -translate-y-1/2 rotate-45 border-r-2 border-b-2 border-body-color'></span>
+        </div>
+      </>
+    )
+  }
 
-const SituationMatriomoniale = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Situation Matrimoniale
-      </label>
-      <div className='relative z-20'>
-        <select className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'>
-          <option value='' className='dark:bg-dark-2'>Marié</option>
-          <option value='' className='dark:bg-dark-2'>Célibataire</option>
-          <option value='' className='dark:bg-dark-2'>Autre</option>
-        </select>
-        <span className='absolute right-4 top-1/2 z-10 mt-[-2px] h-[10px] w-[10px] -translate-y-1/2 rotate-45 border-r-2 border-b-2 border-body-color'></span>
-      </div>
-    </>
-  )
-}
+  const SituationMatriomoniale = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Situation Matrimoniale
+        </label>
+        <div className='relative z-20'>
+          <select className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'>
+            <option value='' className='dark:bg-dark-2'>Marié</option>
+            <option value='' className='dark:bg-dark-2'>Célibataire</option>
+            <option value='' className='dark:bg-dark-2'>Autre</option>
+          </select>
+          <span className='absolute right-4 top-1/2 z-10 mt-[-2px] h-[10px] w-[10px] -translate-y-1/2 rotate-45 border-r-2 border-b-2 border-body-color'></span>
+        </div>
+      </>
+    )
+  }
 
-const Email = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Email
-      </label>
-      <input
-        type='text'
-        placeholder='Your mail'
-        className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
-      />
-    </>
-  )
-}
+  const Email = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Email
+        </label>
+        <input
+          type='text'
+          placeholder='Your mail'
+          className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
+        />
+      </>
+    )
+  }
 
-const PhoneNumber = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Numero de Telephone
-      </label>
-      <input
-        type='int'
-        placeholder='Your number'
-        className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
-      />
-    </>
-  )
-}
+  const PhoneNumber = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Numero de Telephone
+        </label>
+        <input
+          type='int'
+          placeholder='Your number'
+          className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
+        />
+      </>
+    )
+  }
 
-const Profession = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Profession
-      </label>
-      <input
-        type='text'
-        placeholder='Your Profession'
-        className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
-      />
-    </>
-  )
-}
+  const Profession = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Profession
+        </label>
+        <input
+          type='text'
+          placeholder='Your Profession'
+          className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
+        />
+      </>
+    )
+  }
 
-const Birthday = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Date de naissance:
-      </label>
-      <input
-        type='date'
-        placeholder='Birthday'
-        className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
-      />
-    </>
-  )
-}
+  const Birthday = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Date de naissance:
+        </label>
+        <input
+          type='date'
+          placeholder='Birthday'
+          className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
+        />
+      </>
+    )
+  }
 
-const LieuNais = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Lieu de naissance
-      </label>
-      <input
-        type='text'
-        placeholder='LieuNais'
-        className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
-      />
-    </>
-  )
-}
+  const LieuNais = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Lieu de naissance
+        </label>
+        <input
+          type='text'
+          placeholder='LieuNais'
+          className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
+        />
+      </>
+    )
+  }
 
-const Genre = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Genre
-      </label>
-      <div className='relative z-20'>
-        <select className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'>
-          <option value='' className='dark:bg-dark-2'>Masculin</option>
-          <option value='' className='dark:bg-dark-2'>Feminin</option>
-          <option value='' className='dark:bg-dark-2'>Autre</option>
-        </select>
-        <span className='absolute right-4 top-1/2 z-10 mt-[-2px] h-[10px] w-[10px] -translate-y-1/2 rotate-45 border-r-2 border-b-2 border-body-color'></span>
-      </div>
-    </>
-  )
-}
-const DadName = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Nom complet du pere:
-      </label>
-      <input
-        type='text'
-        placeholder='DadName'
-        className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
-      />
-    </>
-  )
-}
+  const Genre = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Genre
+        </label>
+        <div className='relative z-20'>
+          <select className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'>
+            <option value='' className='dark:bg-dark-2'>Masculin</option>
+            <option value='' className='dark:bg-dark-2'>Feminin</option>
+            <option value='' className='dark:bg-dark-2'>Autre</option>
+          </select>
+          <span className='absolute right-4 top-1/2 z-10 mt-[-2px] h-[10px] w-[10px] -translate-y-1/2 rotate-45 border-r-2 border-b-2 border-body-color'></span>
+        </div>
+      </>
+    )
+  }
+  const DadName = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Nom complet du pere:
+        </label>
+        <input
+          type='text'
+          placeholder='DadName'
+          className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
+        />
+      </>
+    )
+  }
 
-const Lieu = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Lieu de naissance
-      </label>
-      <input
-        type='text'
-        placeholder='LieuNais'
-        className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
-      />
-    </>
-  )
-}
+  const Lieu = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Lieu de naissance
+        </label>
+        <input
+          type='text'
+          placeholder='LieuNais'
+          className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
+        />
+      </>
+    )
+  }
 
-const Genr = () => {
-  return (
-    <>
-      <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
-        Date 
-      </label>
-      <div className='relative z-20'>
-        <select className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
-         text-black outline-none transition focus:border-primary active:border-primary
-          disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'>
-          <option value='' className='dark:bg-dark-2'>Masculin</option>
-          <option value='' className='dark:bg-dark-2'>Feminin</option>
-          <option value='' className='dark:bg-dark-2'>Autre</option>
-        </select>
-        <span className='absolute right-4 top-1/2 z-10 mt-[-2px] h-[10px] w-[10px] -translate-y-1/2 rotate-45 border-r-2 border-b-2 border-body-color'></span>
-      </div>
-    </>
-  )
-}
+  const DeathDate = () => {
+    return (
+      <>
+        <label className='mb-[10px] block text-base font-medium text-dark dark:text-black text-left'>
+          Date de deces
+        </label>
+        <input
+          type='date'
+          placeholder='Date de deces'
+          className='w-full bg-gray-200 border border-stroke dark:border-black py-[10px] px-5
+          text-black outline-none transition focus:border-primary active:border-primary
+            disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2'
+        />
+      </>
+    )
+  }
